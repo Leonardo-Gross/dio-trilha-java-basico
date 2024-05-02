8acb90992ac80857974463924c1df763c02f1708
Leonardo <leogrossms@gmail.com>
2024-05-02 16:00:45 -0300

    criando metodos para uma SmartTv

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,22 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //Tipos Primitivos
+        //Estudar classe String
+
+        String meuNome = "Leonarddo Gross";
+        
+        double salarioMinimo = 1900;
+
+        short numeroCurto = 1;
+        int numeroNomrmal = numeroCurto;
+        short numeroCurto2 = (short) numeroNomrmal;
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;
     }
 }
